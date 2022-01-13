a30e4764d5e5973a5648070fd2d4d0ebd364598d
Tiago Almeida <tiagodalmeida87@hotmail.com>
2022-01-13 00:04:51 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>Página Principal</h1>
+    <Navbar />
   );
 }
 
