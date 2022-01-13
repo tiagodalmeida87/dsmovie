304dc2879a1346795e6ed57a28df861644d13314
Tiago Almeida <tiagodalmeida87@hotmail.com>
2022-01-13 00:12:29 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form() {
+    return (
+        <h1>pagina de Formulário</h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing() {
+    return (
+        <h1>pagina de listgem</h1>
+    );
+}
+
+export default Listing;
